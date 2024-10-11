e5e6c3394d5e228836174fd25af87b3adf50c1c2
chela <chela@localhost>
2024-10-11 07:29:41 -0400

    comienzo de la interfaz grafica

---
+++ NetBeansProjects/proy1EDD/src/proy1edd/Proy1EDD.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proy1edd;
+import proy1edd.interfaces.InterfazUnica;
+
+/**
+ *
+ * @author chela
+ */
+public class Proy1EDD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        InterfazUnica proy1 = new InterfazUnica();
+        proy1.show();
+    }
+    
+}
