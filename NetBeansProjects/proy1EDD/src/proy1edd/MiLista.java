5e812235917e01eb1e50bf52fe0ec5f60e02fb3a
chela <chela@localhost>
2024-10-13 14:07:03 -0400

    lista de adyacencia

---
+++ NetBeansProjects/proy1EDD/src/proy1edd/MiLista.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proy1edd;
+
+/**
+ *
+ * @author chela
+ */
+public class MiLista {
+    private String[] data;
+    private int size;
+    private int capacity;
+    
+    public MiLista() {
+        capacity = 10;
+        data = new String[capacity];
+        size = 0;
+    }
+    
+    //Añade elementos a la lista
+    public void add(String value) {
+        if (size == capacity) {
+            resize();
+        }
+        data[size++] = value;
+    }
+    
+    //Obtiene elemento en un índice específico
+    public String get(int index) {
+        if (index >= 0 && index < size) {
+            return data[index];
+        }
+        return null;
+    }
+    
+    //Ajusta el tamaño de la lista cuando llega a su capacidad máxima
+    private void resize() {
+        capacity *= 2;
+        String[] newData = new String[capacity];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
+    }
+    
+    //Obtiene el tamaño actual de la lista
+    public int size() {
+        return size;
+    }
+    
+    
+}
--- NetBeansProjects/proy1EDD/src/proy1edd/interfaces/InterfazInteractuar.java
+++ NetBeansProjects/proy1EDD/src/proy1edd/interfaces/InterfazInteractuar.java
@@ -3,7 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package proy1edd.interfaces;
-
+import javax.swing.JFileChooser;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 /**
  *
  * @author chela
@@ -29,13 +32,12 @@ public class InterfazInteractuar extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
         jLabel3 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
         jTextArea2 = new javax.swing.JTextArea();
         jLabel4 = new javax.swing.JLabel();
+        jFileChooser1 = new javax.swing.JFileChooser();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(255, 204, 255));
@@ -46,34 +48,39 @@ public class InterfazInteractuar extends javax.swing.JFrame {
 
         jLabel1.setFont(new java.awt.Font("Savoye LET", 0, 24)); // NOI18N
         jLabel1.setText("Ubicación de Sucursales");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 10, -1, 40));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 20, -1, 40));
 
-        jLabel2.setText("Ingrese el link con la informacion de transporte de su ciudad:");
+        jLabel2.setText("Ingrese el archivo JSON con la información de transporte de su ciudad:");
         getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 70, -1, -1));
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane1.setViewportView(jTextArea1);
-
-        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 90, 440, 30));
-
         jLabel3.setText("Ingrese la distancia \"t\" que debe haber entre sucursales:");
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 130, 320, -1));
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 420, 440, -1));
 
         jLabel5.setText("t =");
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 160, -1, -1));
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 460, -1, -1));
 
         jTextArea2.setColumns(20);
         jTextArea2.setRows(5);
         jScrollPane2.setViewportView(jTextArea2);
 
-        getContentPane().add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 150, 410, 30));
+        getContentPane().add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 450, 410, 30));
 
-        jLabel4.setText("Ensenar grafo:");
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 200, -1, -1));
+        jLabel4.setText("Enseñar grafo:");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 500, -1, -1));
+
+        jFileChooser1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jFileChooser1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jFileChooser1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 90, 670, 330));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jFileChooser1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jFileChooser1ActionPerformed
+        
+    }//GEN-LAST:event_jFileChooser1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -111,15 +118,14 @@ public class InterfazInteractuar extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JFileChooser jFileChooser1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTextArea jTextArea1;
     private javax.swing.JTextArea jTextArea2;
     // End of variables declaration//GEN-END:variables
 }
